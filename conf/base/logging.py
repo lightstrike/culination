d3a273f7a64a82e83ec3c2991c798d1663364d78
Andrew Bates <andrew.bates@tutelageinc.com>
2013-03-27 00:26:44 -0500

    turned base conf into a package moved logging into it's own file
    Signed-off-by: Andrew Bates <andrew.bates@tutelageinc.com>

---
+++ conf/base/logging.py
@@ -0,0 +1,41 @@
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': {
+        'standard': {
+            'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s'
+        },
+    },
+    'handlers': {
+        'default': {
+            'level':'DEBUG',
+            'class':'logging.handlers.RotatingFileHandler',
+            'filename': 'production/logs/django.log', # FIXME(andrew)
+            'maxBytes': 1024*1024*50,
+            'backupCount': 5,
+            'formatter':'standard',
+        },  
+        'request_handler': {
+                'level':'DEBUG',
+                'class':'logging.handlers.RotatingFileHandler',
+		# FIXME(andrew)
+                'filename': 'production/logs/django_request.log',
+                'maxBytes': 1024*1024*5,
+                'backupCount': 5,
+                'formatter':'standard',
+        },
+    },
+    'loggers': {
+
+        '': {
+            'handlers': ['default'],
+            'level': 'DEBUG',
+            'propagate': True
+        },
+        'django.request': {
+            'handlers': ['request_handler'],
+            'level': 'DEBUG',
+            'propagate': False
+        },
+    }
+}
